670fba43630a2e2f74142703bbb8bdb6539d7c4e
Atit Kharel <atit286@gmail.com>
2022-07-08 15:57:50 +0545

    inital commit

---
+++ index.js
@@ -0,0 +1,20 @@
+$ = (queryString) => document.querySelector(queryString);
+
+const shiftHue = (hue) => (hue + 0.1) % 360;
+
+let hue = 0;
+
+const animate = () => {
+    hue = shiftHue(hue);
+    const color = `hsl(${hue}, 100%, 50%)`;
+    $('#box2').setAttribute('color', color);
+
+    //animate position of box2
+    const position = `-1 ${2 + Math.sin(Date.now() / 1000)} -5`;
+    $('#box2').setAttribute('position', position);
+
+    requestAnimationFrame(animate);
+}
+
+
+requestAnimationFrame(animate);
