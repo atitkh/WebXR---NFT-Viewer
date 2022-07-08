9036e20ea4df305182e968cdb774fb6d37ef5c64
Atit Kharel <atit286@gmail.com>
2022-07-08 16:08:50 +0545

    added image  texture

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let hue = 0;
 const animate = () => {
     hue = shiftHue(hue);
     const color = `hsl(${hue}, 100%, 50%)`;
-    $('#box2').setAttribute('color', color);
+    // $('#box2').setAttribute('color', color);
 
     //animate position of box2
     const position = `-1 ${2 + Math.sin(Date.now() / 1000)} -5`;
@@ -16,5 +16,10 @@ const animate = () => {
     requestAnimationFrame(animate);
 }
 
+// function to change texture of plane
+const changeTexture = (objectID,texture) => {
+    $(objectID).setAttribute('src', texture);
+}
 
+changeTexture('#image1', "https://static.remove.bg/remove-bg-web/5c20d2ecc9ddb1b6c85540a333ec65e2c616dbbd/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg");
 requestAnimationFrame(animate);
