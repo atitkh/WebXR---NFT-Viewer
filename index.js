3417bca2ed1f6c80fa6fd323be6725d4f43d80a6
Atit Kharel <atit286@gmail.com>
2022-07-10 15:14:02 +0545

    added image and changed positions

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ const changeTexture = (objectID,texture) => {
     $(objectID).setAttribute('src', texture);
 }
 
-changeTexture('#image1', "https://static.remove.bg/remove-bg-web/5c20d2ecc9ddb1b6c85540a333ec65e2c616dbbd/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg");
+changeTexture('#image1', "assets/vs-bot.png");
 requestAnimationFrame(animate);
