dae7a6a5b4718e3f58620755c1ad3dc44629d963
Atit Kharel <atit286@gmail.com>
2022-07-10 14:43:57 +0545

    added meta mask connection

---
+++ web3.js
@@ -0,0 +1,48 @@
+var account = null;
+let currentAccount = null
+$ = (queryString) => document.querySelector(queryString);
+
+//anonymous async function to initialize await values
+(async () => {
+	//connectWallet function to connect wallet and get account address
+	$('#connectButton').addEventListener('click', function (evt) {
+        connectWallet();
+      });
+})();
+
+function handleAccountsChanged(accounts) {
+  if (accounts.length === 0) {
+    // MetaMask is locked or the user has not connected any accounts
+    console.log('Please connect to MetaMask.');
+  } else if (accounts[0] !== currentAccount) {
+    currentAccount = accounts[0];
+    accBalance = 0;
+    hiddenAddress = currentAccount.substring(0, 5) + "..." + currentAccount.substring(currentAccount.length - 4, currentAccount.length);
+    changeAttribute('#wallet-address', 'value', "Account: " + hiddenAddress);
+    changeAttribute('#wallet-balance', 'value', "Balance: " + (accBalance / (10 ** 18)).toFixed(4));
+  }
+}
+
+function handleChainChanged(_chainId) {
+  // We recommend reloading the page, unless you must do otherwise
+  window.location.reload();
+}
+
+function connectWallet() {
+    ethereum
+      .request({ method: 'eth_requestAccounts' })
+      .then(handleAccountsChanged)
+      .catch((err) => {
+        if (err.code === 4001) {
+          // EIP-1193 userRejectedRequest error
+          // If this happens, the user rejected the connection request.
+          console.log('Please connect to MetaMask.');
+        } else {
+          console.error(err);
+        }
+      });
+}
+
+const changeAttribute = (objectID,attribue,value) => {
+    $(objectID).setAttribute(attribue, value);
+}
